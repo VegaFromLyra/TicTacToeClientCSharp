a009b3e97a26f049b06edd5ae0bec3b8524d4549
VegaFromLyra <asha.ab@gmail.com>
2014-02-25 18:08:12 -0800

    Tic tac toe for a board of size 3

---
+++ TicTacToeClientCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeClientCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe game = new TicTacToe();
+
+            Piece[,] input1 = new Piece[,] {
+                                             {Piece.X, Piece.X, Piece.X},
+                                             {Piece.O, Piece.X, Piece.O},
+                                             {Piece.X, Piece.O, Piece.O}
+                                          };
+            game.Init(input1);
+            Console.WriteLine("Test case 1: Winner is {0}", game.hasWon());
+
+
+            Piece[,] input2 = new Piece[,] {
+                                             {Piece.O, Piece.X, Piece.X},
+                                             {Piece.O, Piece.X, Piece.O},
+                                             {Piece.O, Piece.O, Piece.X}
+                                          };
+            game.Init(input2);
+            Console.WriteLine("Test case 2: Winner is {0}", game.hasWon());
+
+
+            Piece[,] input3 = new Piece[,] {
+                                             {Piece.X, Piece.O, Piece.X},
+                                             {Piece.O, Piece.X, Piece.O},
+                                             {Piece.O, Piece.O, Piece.X}
+                                          };
+            game.Init(input3);
+            Console.WriteLine("Test case 3: Winner is {0}", game.hasWon());
+
+            Piece[,] input4 = new Piece[,] {
+                                             {Piece.X, Piece.O, Piece.O},
+                                             {Piece.X, Piece.O, Piece.O},
+                                             {Piece.O, Piece.O, Piece.X}
+                                          };
+            game.Init(input4);
+            Console.WriteLine("Test case 4: Winner is {0}", game.hasWon());
+        }
+    }
+}
---
+++ TicTacToeClientCSharp/TicTacToe.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeClientCSharp
+{
+    public enum Piece
+    {
+        Empty,
+        O,
+        X
+    };
+
+    class TicTacToe
+    {
+
+        private Piece[,] board;
+
+        static private int N = 3;
+
+        public void Init(Piece[,] board)
+        {
+            this.board = board;
+        }
+
+        public Piece hasWon()
+        {
+            // Check rows
+            for (int i = 0; i < N; i++)
+            {
+                if (board[i, 0] != Piece.Empty &&
+                    board[i, 0] == board[i, 1] &&
+                    board[i, 1] == board[i, 2])
+                {
+                    return board[i, 0];
+                }
+            }
+
+            // Check columns
+            for (int i = 0; i < N; i++)
+            {
+                if (board[0, i] != Piece.Empty &&
+                    board[0, i] == board[1, i] &&
+                    board[1, i] == board[2, i])
+                {
+                    return board[0, i];
+                }
+            }
+
+            // Check Diagonal 1
+            if (board[0, 0] != Piece.Empty &&
+                board[0, 0] == board[1, 1] &&
+                board[1, 1] == board[2, 2])
+            {
+                return board[0, 0];
+            }
+
+            // Check diagonal 2
+            if (board[2, 0] != Piece.Empty &&
+                board[2, 0] == board[1, 1] &&
+                board[1, 1] == board[0, 2])
+            {
+                return board[2, 0];
+            }
+
+            return Piece.Empty;
+        }
+    }
+}
